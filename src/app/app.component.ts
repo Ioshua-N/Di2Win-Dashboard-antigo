2aad8e56e9de5deff0d91a40654b0bce827da6fe
Ioshua <126164539+Ioshua-N@users.noreply.github.com>
2023-11-18 21:49:03 -0300

    responsive sidebar
    responsiva para pcs e mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MainComponent, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Di2Win-Dashboard';
+
+export class AppComponent 
+{
+  title = 'Dashboard';
 }
